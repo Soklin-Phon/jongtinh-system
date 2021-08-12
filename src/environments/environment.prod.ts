4b67559f22352ace438c3c5eef6d44ba5b62c778
Soklin <phonsoklin670@gmail.com>
2021-08-12 19:01:11 +0700

    new code

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  hmr: false, 
+  token: 'cp-token', 
+
+
+  // ===============================================>> Development
+  apiUrl      : 'http://api.jongtinh.com/api',
+  fileUrl     : 'http://file.server.jongtinh.com/',
+  url         : 'http://system.server.jongtinh.com/',
+
+  user:       '',
+  password:   ''
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  hmr: false, 
+  token: 'cp-token',
+  
+  apiUrl      : 'http://127.0.0.1:8000/api',
+  fileUrl     : 'http://127.0.0.1:7000',
+  url         : 'http://localhost:8080/',
+  
+  
+  // apiUrl      : 'http://api.jongtinh.com/api',
+  // fileUrl     : 'http://file.jongtinh.com/',
+  // url         : 'http://localhost:8080/',
+  
+  user:       '',
+  password:   ''
+};
